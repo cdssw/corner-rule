0859829313f24b6d5a92ad03c79910abc7383f80
cdssw <44996691+cdssw@users.noreply.github.com>
2020-07-13 13:42:24 +0900

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,7 +7,7 @@ import Button from '@material-ui/core/Button';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import { loginCall } from "../services/Authorization";
 
 const useStyles = makeStyles((theme) => ({
@@ -70,6 +70,7 @@ const useStyles = makeStyles((theme) => ({
 export default function Login() {
   const classes = useStyles();
 
+  const [isAuth, setIsAuth] = useState(false);
   const [checkId, setCheckId] = useState(false);
   const [login, setLogin] = useState({
     username: null,
@@ -91,9 +92,10 @@ export default function Login() {
     const response = await loginCall(login);
     console.log(response);
     alert(response.data.access_token);
-    this.props.history.push("/");
+    setIsAuth(true);
   }
 
+  if(isAuth) return <Redirect to='/' />
   return (
     <Container className={classes.main} component="main">
       <Link to="/">
