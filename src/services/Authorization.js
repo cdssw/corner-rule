d5a5b8a8dec6ed5833fd71b9cc817f5e8e1a553e
cdssw <44996691+cdssw@users.noreply.github.com>
2020-07-13 09:57:50 +0900

    Update Authorization.js

--- src/services/Authorization.js
+++ src/services/Authorization.js
@@ -1,8 +1,7 @@
 import axios from "axios";
-import { Base64 } from "js-base64";
 import querystring from "querystring";
 
-export function loginCall(props) {
+export async function loginCall(props) {
   const url = 'https://cdssw.duckdns.org:9015/';
   const data = {
     grant_type: 'password',
@@ -11,15 +10,15 @@ export function loginCall(props) {
     common: 'common',
     ...props
   };
-  axios({
-    url: '/oauth/token',
-    baseURL: url,
-    method: 'post',
-    auth: { username: 'auth_id', password: 'auth_secret'},
-    data: querystring.stringify(data)
-  }).then(response => {
-    console.log(response.data);
-  }).catch(err => {
-    console.log(err);
-  });
+  try {
+    return axios({
+      url: '/oauth/token',
+      baseURL: url,
+      method: 'post',
+      auth: { username: 'auth_id', password: 'auth_secret'},
+     data: querystring.stringify(data)
+    });
+  } catch(error) {
+    console.log(error);
+  }
 }
