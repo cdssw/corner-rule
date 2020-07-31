4634a9b80a0b3e5e0ae12e71b16e3ea2e5f72b3a
cdssw <44996691+cdssw@users.noreply.github.com>
2020-07-31 16:32:05 +0900

    Update index.js

--- src/components/organisms/SignupForm/index.js
+++ src/components/organisms/SignupForm/index.js
@@ -182,8 +182,14 @@ export default function SignupForm(props) {
   const { emailConfirm, userNickNmConfirm } = props.state.boolean;
 
   const handleNext = () => {
-    if (activeStep === 0 && !emailConfirm) alert('이메일ID 중복확인을 하세요.');
-    if (activeStep === 1 && !userNickNmConfirm) alert('닉네임 중복확인을 하세요.');
+    if (activeStep === 0 && !emailConfirm) {
+      alert('이메일ID 중복확인을 하세요.');
+      return;
+    }
+    if (activeStep === 1 && !userNickNmConfirm) {
+      alert('닉네임 중복확인을 하세요.');
+      return;
+    }
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
   };
 
