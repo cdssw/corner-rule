f695a76859fa704b2d19bb2467cea291e7c35647
dw choi <cdssw@naver.com>
2021-04-14 10:33:52 +0900

    주석추가

--- src/services/Juso.js
+++ src/services/Juso.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const url = process.env.REACT_APP_SERVICE_JUSO
+// 발급시 검색용API로 발급해야 함.
 const key = 'devU01TX0FVVEgyMDIxMDQxNDEwMzIxMTExMTA0ODQ=';
 
 export async function postJuso(props) {
